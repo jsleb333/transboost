c0ac16ae0cc44cb7c40d68e4e1e612d59458b579
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2019-06-10 11:55:53 -0400

    bug fix with imports

--- transboost/datasets/__init__.py
+++ transboost/datasets/__init__.py
@@ -1,5 +1,4 @@
 try:
     from transboost.datasets.datasets import ImageDataset, MNISTDataset, CIFAR10Dataset
-    import datasets
 except ModuleNotFoundError:
     from .datasets import ImageDataset, MNISTDataset, CIFAR10Dataset
