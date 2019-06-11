2bc7b7a82446ce7cdb8ef9f77e09979a7e4ec0f0
Léandre Gagnon-Lewis <leandregagnonlewis@gmail.com>
2019-06-11 16:36:57 -0400

    test templates in place

--- tests/test_transboost.py
+++ tests/test_transboost.py
@@ -2,4 +2,14 @@ import pytest
 
 
 class Testtransboost:
-    pass
+    def test_get_multi_layers_random_features(self):
+        pass
+
+    def test_advance_to_the_next_layer(self):
+        pass
+
+    def test_draw_filters_from_bank(self):
+        pass
+
+    def test_init_filters(self):
+        pass
