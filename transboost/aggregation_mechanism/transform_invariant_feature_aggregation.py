9ee5fff3aa6ccf7585d6237fa41870b62ee956c8
jsleb333 <jean-samuel.leboeuf.1@ulaval.ca>
2019-06-12 18:00:29 -0400

    refactor transform weights

--- transboost/aggregation_mechanism/transform_invariant_feature_aggregation.py
+++ transboost/aggregation_mechanism/transform_invariant_feature_aggregation.py
@@ -45,19 +45,9 @@ class TransformInvariantFeatureAggregation:
             i_max = min(i + weights.shape[-2] + self.locality, height)
             j_max = min(j + weights.shape[-1] + self.locality, width)
 
-            transformed_weights = []
-            for affine_transforms in ats:
-                transformed_chs = []
-                for ch, affine_transform in zip(weights, affine_transforms):
-                    transformed_ch = affine_transform(ch)
-                    transformed_ch = torch.unsqueeze(torch.from_numpy(transformed_ch), dim=0)
-                    transformed_chs.append(transformed_ch)
-                transformed_chs = torch.unsqueeze(torch.cat(transformed_chs, dim=0), dim=0)
-                transformed_weights.append(transformed_chs)
-
-            transformed_weights = torch.cat(transformed_weights, dim=0)
+            transformed_weights = self._transform_weights(weights, ats)
             transformed_weights.to(device=X.device)
-            # transformed_weights.shape = (n_transforms, n_channel, filter_height+4, filter_width+4)
+            # transformed_weights.shape = (n_transforms, n_channel, filter_height+pad, filter_width+pad)
 
             output = F.conv2d(X[:,:,i_min:i_max, j_min:j_max], transformed_weights)
             # output.shape = (n_examples, n_transforms, height, array)
@@ -74,6 +64,19 @@ class TransformInvariantFeatureAggregation:
             high_level_features = self.activation(high_level_features)
         high_level_features = high_level_features.cpu().reshape((n_examples, -1))
         return high_level_features
+
+    def _transform_weights(self, weights, affine_transforms):
+        transformed_weights = []
+        for affine_transforms_ch in affine_transforms:
+            transformed_chs = []
+            for ch, affine_transform in zip(weights, affine_transforms_ch):
+                transformed_ch = affine_transform(ch)
+                transformed_ch = torch.unsqueeze(torch.from_numpy(transformed_ch), dim=0)
+                transformed_chs.append(transformed_ch)
+            transformed_chs = torch.unsqueeze(torch.cat(transformed_chs, dim=0), dim=0)
+            transformed_weights.append(transformed_chs)
+
+        return torch.cat(transformed_weights, dim=0)
 
     def _compute_maxpool_shape(self, output):
         if self.maxpool_shape[0] == -1:
