8848f6606bc043e1ac8d3d6b8b7b017eec68a900
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2019-06-26 11:05:02 -0400

    merge

--- tests/n_transform_test.py
+++ tests/n_transform_test.py
@@ -10,66 +10,6 @@ from sklearn.metrics import accuracy_score
 from transboost.label_encoder import OneHotEncoder
 
 from transboost.datasets import MNISTDataset, get_train_valid_test_bank
-<<<<<<< HEAD
-from transboost.transboost_v2 import TransBoost, advance_to_the_next_layer, \
-    MultiLayersRandomFeatures, get_multi_layers_filters, WLRidge
-from transboost.utils import make_fig_axes, FiltersGenerator
-
-
-(Xtr, Ytr), (X_val, Y_val), (Xts, Yts), filter_bank = get_train_valid_test_bank(
-    dataset='mnist',
-    valid=200,
-    center=True,
-    reduce=True,
-    shuffle=101,
-    n_examples=1000,
-    bank_ratio=0.05,
-    # device='cuda'
-)
-
-amount_of_transformations = range(5, 10, 5)
-train_accuracies = []
-val_accuracies = []
-encoder = OneHotEncoder(Ytr)
-weak_learner = WLRidge(encoder)
-# encoded_Y, weights = encoder.encode_labels(Ytr)
-np.random.seed(101)
-for nt in amount_of_transformations:
-    print(f'Beginning calculations for {nt} transformations')
-    temp_tr = []
-    temp_val = []
-    for i in range(10):
-        print(f'iteration: {i}')
-        # generate filters
-        filters_generator = FiltersGenerator(filter_bank, filters_shape=(5, 5), rotation=15, scale=.1, shear=15, n_transforms=nt, margin=2)
-        filters = get_multi_layers_filters(filters_generator, [100])
-        # generate attribute and train weak learner
-
-        aggregation_mechanism = MultiLayersRandomFeatures(locality=3, maxpool_shape=(-1,-1,-1))
-        S_tr = aggregation_mechanism(Xtr, filters)
-
-        weak_predictor = weak_learner().fit(S_tr, Ytr)
-        # calculate train accuracy
-        # weak_prediction = weak_predictor.predict(S_tr)
-        # Y_pred = encoder.decode_labels(weak_prediction)
-        train_acc = weak_predictor.evaluate(y_true=Ytr, y_pred=Y_pred)
-        temp_tr.append(train_acc)
-        # calculate validation accuracy
-        # S_val = aggregation_mechanism(X_val, filters)
-        # weak_prediction_val = weak_predictor.predict(S_val)
-        # Y_val_pred = encoder.decode_labels(weak_prediction_val)
-        # val_acc = accuracy_score(y_true=Y_val, y_pred=Y_val_pred)
-        # temp_val.append(val_acc)
-    train_accuracies.append(np.mean(temp_tr))
-    # val_accuracies.append(np.mean(temp_val))
-
-# plot the data
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-ax.plot([amount_of_transformations], train_accuracies,  color='tab:blue')
-# ax.plot([amount_of_transformations], val_accuracies, color='tab:orange')
-plt.show()
-=======
 from transboost.transboost_v2 import get_multi_layers_filters, WLRidge, MultiLayersRandomFeatures
 from transboost.utils import FiltersGenerator
 from graal_utils import Timer
@@ -177,7 +117,3 @@ def run(min_number_of_iterations, minim, maxim, step, criterion, m, val, dataset
     plt.show()
 
     _run.add_artifact('n_transforms.csv')
-
-
-
->>>>>>> 177a39e58977ba8b848d28fd628056d60d35adf3
