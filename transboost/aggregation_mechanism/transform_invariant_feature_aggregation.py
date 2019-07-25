61ae3054e7d093de744b832eb47f2fcbf3deac18
Léandre Gagnon-Lewis <leandregagnonlewis@gmail.com>
2019-07-25 13:33:00 -0400

    comment division par determinant

--- transboost/aggregation_mechanism/transform_invariant_feature_aggregation.py
+++ transboost/aggregation_mechanism/transform_invariant_feature_aggregation.py
@@ -108,7 +108,7 @@ class TransformInvariantFeatureAggregation:
             transformed_chs = []
             for ch, affine_transform in zip(weights, affine_transforms_ch):
                 affine_transform.center += pad
-                transformed_ch = affine_transform(np.pad(ch, pad, 'constant')) / affine_transform.determinant
+                transformed_ch = affine_transform(np.pad(ch, pad, 'constant')) # / affine_transform.determinant
                 transformed_ch = torch.unsqueeze(torch.from_numpy(transformed_ch), dim=0)
                 transformed_chs.append(transformed_ch)
             transformed_chs = torch.unsqueeze(torch.cat(transformed_chs, dim=0), dim=0)
